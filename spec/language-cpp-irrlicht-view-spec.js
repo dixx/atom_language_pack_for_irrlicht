2b7e151fd9bba6bfeb515513bd1809c0a133e89c
jenzdixx <jenzdixx@arcor.de>
2016-05-11 20:02:39 +0200

    initial commit

---
+++ spec/language-cpp-irrlicht-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageCppIrrlichtView from '../lib/language-cpp-irrlicht-view';
+
+describe('LanguageCppIrrlichtView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
