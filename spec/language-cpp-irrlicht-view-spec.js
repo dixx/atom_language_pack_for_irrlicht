9e185da725c6ea6837587c043c172316283619c9
dixx <jenzdixx@googlemail.com>
2016-05-12 12:57:45 +0200

    Remove unnecessary files

--- spec/language-cpp-irrlicht-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageCppIrrlichtView from '../lib/language-cpp-irrlicht-view';
-
-describe('LanguageCppIrrlichtView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
